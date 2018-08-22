319f6cf5afab90f48f727e0b059cfd9e3e4f1ed9
MADMAX\Max <admaks008@gmail.com>
2018-08-22 17:31:07 +0300

    Fifteen's Game

---
+++ Fifteen/Cell.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Fifteen
+{
+    public class Cell : Control
+    {
+        static Cell()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Cell), new FrameworkPropertyMetadata(typeof(Cell)));
+        }
+
+        #region Text...
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(Cell), new UIPropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        #endregion
+
+        #region Top...
+        public static readonly DependencyProperty TopProperty = DependencyProperty.Register("Top", typeof(double), typeof(Cell), new UIPropertyMetadata(default(double)));
+        public double Top
+        {
+            get { return (double)GetValue(TopProperty); }
+            set { SetValue(TopProperty, value); }
+        }
+        #endregion
+        #region Left...
+        public static readonly DependencyProperty LeftProperty = DependencyProperty.Register("Left", typeof(double), typeof(Cell), new UIPropertyMetadata(default(double)));
+        public double Left
+        {
+            get { return (double)GetValue(LeftProperty); }
+            set { SetValue(LeftProperty, value); }
+        }
+
+        #endregion
+
+        #region Row...
+        public static readonly DependencyProperty RowProperty = DependencyProperty.Register("Row", typeof(int), typeof(Cell), new FrameworkPropertyMetadata(default(int), OnRowChanged));
+        public int Row
+        {
+            get { return (int)GetValue(RowProperty); }
+            set { SetValue(RowProperty, value); }
+        }
+        private static void OnRowChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            var instance = source as Cell;
+            if (instance == null) return;
+            instance.AnimateMove(TopProperty, (int)e.NewValue);
+        }
+
+        #endregion
+        #region Column...
+        public static readonly DependencyProperty ColumnProperty = DependencyProperty.Register("Column", typeof(int), typeof(Cell), new FrameworkPropertyMetadata(default(int), OnColumnChanged));
+        public int Column
+        {
+            get { return (int)GetValue(ColumnProperty); }
+            set { SetValue(ColumnProperty, value); }
+        }
+        private static void OnColumnChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            var instance = source as Cell;
+            if (instance == null) return;
+            instance.AnimateMove(LeftProperty, (int)e.NewValue);
+        }
+        #endregion
+
+        private void AnimateMove(DependencyProperty property, double value, double duration = 250)
+        {
+            var doubleAnimation = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromMilliseconds(duration)),
+                To = value * 75 + value * 5
+            };
+            BeginAnimation(property, doubleAnimation);
+        }
+    }
+}
---
+++ Fifteen/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Fifteen
+{
+
+    public partial class MainWindow
+    {
+        private Tuple<int, int> m_emptyCell;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Cells = new ObservableCollection<Cell>();
+            for (var i = 1; i < 16; i++)
+            {
+                var cell = new Cell { Text = string.Format("{0}", i) };
+                cell.MouseLeftButtonDown += (o, e) => CellClick((Cell)o);
+                Cells.Add(cell);
+            }
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var tuples = new List<Tuple<int, int>>();
+            for (var col = 0; col < 4; col++)
+                for (var row = 0; row < 4; row++)
+                    tuples.Add(new Tuple<int, int>(col, row));
+            var index = 0;
+            var random = new Random(DateTime.Now.Millisecond);
+            do
+            {
+                var tuple = tuples[random.Next(tuples.Count)];
+                var cell = Cells[index];
+                cell.Column = tuple.Item1;
+                cell.Row = tuple.Item2;
+                tuples.Remove(tuple);
+                index++;
+            } while (tuples.Count > 1);
+            m_emptyCell = tuples[0];
+        }
+        private void CellClick(Cell cell)
+        {
+            if (m_emptyCell.Item1 == cell.Column - 1 && m_emptyCell.Item2 == cell.Row)
+            {
+                m_emptyCell = new Tuple<int, int>(cell.Column, m_emptyCell.Item2);
+                cell.Column -= 1;
+            }
+            else if (m_emptyCell.Item1 == cell.Column + 1 && m_emptyCell.Item2 == cell.Row)
+            {
+                m_emptyCell = new Tuple<int, int>(cell.Column, m_emptyCell.Item2);
+                cell.Column += 1;
+            }
+            else if (m_emptyCell.Item1 == cell.Column && m_emptyCell.Item2 == cell.Row - 1)
+            {
+                m_emptyCell = new Tuple<int, int>(m_emptyCell.Item1, cell.Row);
+                cell.Row -= 1;
+            }
+            else if (m_emptyCell.Item1 == cell.Column && m_emptyCell.Item2 == cell.Row + 1)
+            {
+                m_emptyCell = new Tuple<int, int>(m_emptyCell.Item1, cell.Row);
+                cell.Row += 1;
+            }
+
+            CheckResult();
+        }
+
+        private void CheckResult()
+        {
+            //TODO: Must be implemented
+        }
+
+        #region Cells...
+        private static readonly DependencyPropertyKey CellsPropertyKey = DependencyProperty.RegisterReadOnly("Cells", typeof(ObservableCollection<Cell>), typeof(MainWindow), new UIPropertyMetadata(default(ObservableCollection<Cell>)));
+        public static readonly DependencyProperty CellsProperty = CellsPropertyKey.DependencyProperty;
+        public ObservableCollection<Cell> Cells
+        {
+            get { return (ObservableCollection<Cell>)GetValue(CellsProperty); }
+            private set { SetValue(CellsPropertyKey, value); }
+        }
+        #endregion
+    }
+}
