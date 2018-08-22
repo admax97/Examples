75c6bb5b679e59bdcc827300125da6cfb39634d9
MADMAX\Max <admaks008@gmail.com>
2018-08-22 17:14:40 +0300

    MathSquare vs WordTranslate
    ConsoleApp MathSquare, and WordTranslate WPF App that work with Google API

---
+++ ConsoleMathSquare/ConsoleApp1/MathSquare.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, b, c, D, x1, x2;
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("*********Math of square*******************************");
+            Console.WriteLine("******************************************************");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Start1:
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Write for input");
+            Console.ReadLine();
+
+            Console.WriteLine("enter a,b,c");
+            try
+            {
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                c = Convert.ToDouble(Console.ReadLine());
+
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                D = b * b - 4 * a * c;
+
+                if (D >= 0 && a != 0)
+                {
+                    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                    Console.WriteLine("X1 =", x1, "X2 = ", x2);
+                }
+                else
+                {
+                    Console.WriteLine("No square's");
+                    Console.ReadKey();
+                    goto Start1;
+                }
+                Console.ReadLine();
+                Console.ReadKey();
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("You write not allowed number");
+            }
+            finally
+            {
+                Console.WriteLine("Press key for exit");
+                Console.ReadKey();
+            }
+
+
+           
+
+        }
+    }
+}
---
+++ TranslateWord/WpfApp1/translateWord.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string WordToTranslate = TextBox1.Text;
+            string transText;
+
+            HttpWebRequest wrGETURL = (HttpWebRequest)WebRequest.Create("http://translate.google.com.ua/translate_a/t?client=t&text=" + WordToTranslate + "&hl=ru&sl=en&tl=ru&ie=UTF-8&oe=UTF-8&multires=1&prev=btn&ssel=0&tsel=0&sc=1");
+            HttpWebResponse vkHttpWebResponse = (HttpWebResponse)wrGETURL.GetResponse();
+
+            StreamReader vkStreamReader = new StreamReader(vkHttpWebResponse.GetResponseStream());
+            string response = vkStreamReader.ReadToEnd();
+
+            response = response.Replace('[', '1');
+            response = response.Replace('"', '2');
+
+            Regex transTextRegex = new Regex(@"(1112)(\w*)(2)");
+            Match transTextMatch = transTextRegex.Match(response);
+            transText = transTextMatch.Value;
+            transText = transText.Remove(0, 4);
+            transText = transText.Remove(transText.Length - 1, 1);
+
+            TextBox2.Text = transText;
+            
+        }
+    }
+}
